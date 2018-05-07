0598db80041b38ffd31656f67dd253db0113a51c
bitsgalore <johan.vanderknijff@kb.nl>
2018-05-07 14:44:16 +0200

    report relative frequencies

--- analysis.py
+++ analysis.py
@@ -108,6 +108,9 @@ def main():
     errors = pd.Series(np.array(errorsAll))
     warnings = pd.Series(np.array(warningsAll))
 
+    # Number of files
+    noEpubs = len(epubsAll)
+
     fOut.write('\n\n## All EPUBs\n\n')
     fOut.write(dfToMarkdown(epubsAll.describe()))
 
@@ -115,39 +118,59 @@ def main():
     epubsWithErrors = epubsAll[epubsAll.noErrors > 0]
     fOut.write('\n\n## EPUBs with errors\n\n')
     fOut.write(dfToMarkdown(epubsWithErrors.describe()))
+    noEpubsWithErrors = len(epubsWithErrors)
 
     # EPUBs with warnings
     epubsWithWarnings = epubsAll[epubsAll.noWarnings > 0]
     fOut.write('\n\n## EPUBs with warnings\n\n')
     fOut.write(dfToMarkdown(epubsWithWarnings.describe()))
+    noEpubsWithWarnings = len(epubsWithWarnings)
 
     # EPUBs with errors or warnings
     epubsWithErrorsOrWarnings = epubsAll[(epubsAll.noErrors > 0) | (epubsAll.noWarnings > 0)]
     fOut.write('\n\n## EPUBs with errors or warnings\n\n')
     fOut.write(dfToMarkdown(epubsWithErrorsOrWarnings.describe()))
+    noEpubsWithErrorsOrWarnings = len(epubsWithErrorsOrWarnings)
 
     # EPUBs with word count < 1000
     epubsWithWClt1000 = epubsAll[epubsAll.wordCount < 1000]
     fOut.write('\n\n## EPUBs with less than 1000 words\n\n')
     fOut.write(dfToMarkdown(epubsWithWClt1000.describe()))
+    noEpubsWithWClt1000 = len(epubsWithWClt1000)
 
     # Frequency of EPUB versions
     epubVCounts = epubsAll['epubVersion'].value_counts().to_frame()
+
+    # Add column with relative frequencies
+    versionRelFrequencies = []
+    for i, row in epubVCounts.iterrows():
+        relFrequency = 100*row[0]/noEpubs
+        versionRelFrequencies.append(round(relFrequency, 2))
+    
+    epubVCounts.insert(1, '%', versionRelFrequencies)
+
     fOut.write('\n\n## EPUB versions\n\n')
-    fOut.write(dfToMarkdown(epubVCounts,['epubVersion', 'Count']))
+    fOut.write(dfToMarkdown(epubVCounts,['epubVersion', 'Count', '% of all EPUBs']))
 
     # Frequency of errors
     errorCounts = errors.value_counts().to_frame(name="count")
 
-    # Insert column with error descriptions
+    # Insert columns with error descriptions and relative frequencies
     errorDescriptions = []
+    errorRelFrequencies = []
+
     for i, row in errorCounts.iterrows():
         description = messageLookup.get(i, "n/a")
         errorDescriptions.append(description)
+
+        relFrequency = 100*row["count"]/noEpubs
+        errorRelFrequencies.append(round(relFrequency, 2))
+        
     errorCounts.insert(0, 'description', errorDescriptions)
+    errorCounts.insert(2, '%', errorRelFrequencies)
 
     fOut.write('\n\n## Frequency of validation errors\n\n')
-    fOut.write(dfToMarkdown(errorCounts,['Code', 'Description', 'Count']))
+    fOut.write(dfToMarkdown(errorCounts,['Code', 'Description', 'Count', '% of all EPUBs']))
 
     ecPlot = errorCounts.sort_values(by="count").plot(kind='barh',
                               lw=2.5,
@@ -164,15 +187,22 @@ def main():
     # Frequency of warnings
     warningCounts = warnings.value_counts().to_frame(name="count")
 
-    # Insert column with warning descriptions
+    # Insert columns with warning descriptions and relative frequencies
     warningDescriptions = []
+    warningRelFrequencies = []
+
     for i, row in warningCounts.iterrows():
         description = messageLookup.get(i, "n/a")
         warningDescriptions.append(description)
+
+        relFrequency = 100*row["count"]/noEpubs
+        warningRelFrequencies.append(round(relFrequency, 2))
+
     warningCounts.insert(0, 'description', warningDescriptions)
+    warningCounts.insert(2, '%', warningRelFrequencies)
 
     fOut.write('\n\n## Frequency of validation warnings\n\n')
-    fOut.write(dfToMarkdown(warningCounts,['Code', 'Description', 'Count']))
+    fOut.write(dfToMarkdown(warningCounts,['Code', 'Description', 'Count', '% of all EPUBs']))
 
     wcPlot = warningCounts.sort_values(by="count").plot(kind='barh',
                               lw=2.5,
